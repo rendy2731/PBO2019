1b680f065510727a31d508c71370814ebea82457
taufiqgoe <54705540+taufiqgoe@users.noreply.github.com>
2020-03-05 21:24:24 +0800

    Praktikum_02 ~ Fachrizal

---
+++ H071191008/Praktikum_02/Box.java
@@ -0,0 +1,36 @@
+package Tugas.Modul_02;
+
+class Box {
+    private double width;
+    private double heigh;
+    private double depth;
+    private double mass;
+    private double density;
+
+    Box(double width, double heigh, double depth) {
+        this.width = width;
+        this.heigh = heigh;
+        this.depth = depth;
+    }
+
+    public void setWidth(double width) {
+        this.width = width;
+    }
+
+    public void setHeigh(double heigh) {
+        this.heigh = heigh;
+    }
+
+    public void setDepth(double depth) {
+        this.depth = depth;
+    }
+
+    public void setMass(double mass) {
+        this.mass = mass;
+    }
+
+    public double getDensity() {
+        density = mass / (width*heigh*depth);
+        return density;
+    }
+}
